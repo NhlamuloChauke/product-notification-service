7efb12e386bce0ec4a87b0cc245249ff37f2c678
nhlamulo <hlanulani@gmail.com>
2020-02-04 23:29:18 +0200

    code update

--- product-service/src/main/java/com/product/service/AppMain.java
+++ product-service/src/main/java/com/product/service/AppMain.java
@@ -4,7 +4,6 @@ import com.product.service.config.ApplicationProperties;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
-import org.springframework.context.annotation.Configuration;
 
 /**
  * @author nhlamulo
